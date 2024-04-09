1c479ce6eb66f0b35b091a429304271a02c1df01
uwingabir <u.caline@alustudent.com>
2024-04-09 09:52:27 -0700

    py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,100 +1,35 @@
-Importing the function from the module:
->>> matrix_mul = __import__("100-matrix_mul").matrix_mul
+#!/usr/bin/python3
+"""Defines a matrix division function."""
 
-Check: module docstring:
- >>> m = __import__("100-matrix_mul").__doc__
- >>> len(m) > 1
- True
 
-Check: function docstring:
- >>> f = __import__("100-matrix_mul").matrix_mul.__doc__
-         >>> len(f) > 1
-         True
+def matrix_divided(matrix, div):
+    """Divide all elements of a matrix.
 
-Check: no args:
- >>> matrix_mul()
- Traceback (most recent call last):
- ...
- TypeError: matrix_mul() missing 2 required positional arguments: 'm_a' and 'm_b'
+    Args:
+        matrix (list): A list of lists of ints or floats.
+        div (int/float): The divisor.
+    Raises:
+        TypeError: If the matrix contains non-numbers.
+        TypeError: If the matrix contains rows of different sizes.
+        TypeError: If div is not an int or float.
+        ZeroDivisionError: If div is 0.
+    Returns:
+        A new matrix representing the result of the division.
+    """
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
 
-Check: one arg:
- >>> matrix_mul([[1, 2], [3, 4]])
- Traceback (most recent call last):
- ...
- TypeError: matrix_mul() missing 1 required positional argument: 'm_b'
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
 
-Check: too many args:
- >>> matrix_mul([[1, 2], [3, 4]], [[5, 6], [7, 8]], [[9, 10], [11, 12]])
- Traceback (most recent call last):
- ...
- TypeError: matrix_mul() takes 2 positional arguments but 3 were given
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
 
-Check: normal use with ints and floats:
- >>> matrix_mul([[1, 2], [3, 4]], [[1.5, 2.5, 3.5], [4.5, 6.5, 7.5]])
- [[10.5, 15.5, 18.5], [22.5, 33.5, 40.5]]
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
 
-Check: passing m_a as None:
- >>> matrix_mul(None, [[1, 2], [2, 1]])
- Traceback (most recent call last):
- ...
- TypeError: m_a must be a list
-
-Check: passing m_b as None:
- >>> matrix_mul([[1, 2], [2, 1]], None)
- Traceback (most recent call last):
- ...
- TypeError: m_b must be a list
-
-Check: passing empty m_a:
- >>> matrix_mul([], [[1, 2], [2, 1]])
- Traceback (most recent call last):
- ...
- ValueError: m_a can't be empty
-
-Check: passing m_a with empty rows:
- >>> matrix_mul([[], []], [[1, 2], [2, 1]])
- Traceback (most recent call last):
- ...
- ValueError: m_a can't be empty
-
-Check: passing empty m_b:
- >>> matrix_mul([[1, 2], [2, 1]], [])
- Traceback (most recent call last):
- ...
- ValueError: m_b can't be empty
-
-Check: passing m_b with empty rows:
- >>> matrix_mul([[1, 2], [2, 1]], [[]])
- Traceback (most recent call last):
- ...
- ValueError: m_b can't be empty
-
-Check: uneven m_a:
- >>> matrix_mul([[1, 2], [1, 2, 3]], [[1, 2], [2, 1]])
- Traceback (most recent call last):
- ...
- TypeError: each row of m_a must be of the same size
-
-Check: uneven m_b:
- >>> matrix_mul([[1, 2], [2, 1]], [[1, 2], [1, 2, 3]])
- Traceback (most recent call last):
- ...
- TypeError: each row of m_b must be of the same size
-
-Check: string in list in m_a:
- >>> matrix_mul([[1, "Friends"], [2, 1]], [[1, 2], [2, 1]])
- Traceback (most recent call last):
- ...
- TypeError: m_a should contain only integers or floats
-
-Check: string in list in m_b:
- >>> matrix_mul([[1, 2], [2, 1]], [[1, "Friends"], [2, 1]])
- Traceback (most recent call last):
- ...
- TypeError: m_b should contain only integers or floats
-
-Check: mismatching matrices:
- >>> matrix_mul([[1, 1, 1], [1, 1, 1]], [[1, 1], [1, 1,]])
- Traceback (most recent call last):
- ...
- ValueError: m_a and m_b can't be multiplied
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
